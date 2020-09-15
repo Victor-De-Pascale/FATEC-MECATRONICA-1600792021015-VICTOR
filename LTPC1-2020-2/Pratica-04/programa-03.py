27fe84a2c69c55b009fec96ee186008691c67be3
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2020-09-15 08:38:12 -0300

    Create programa-03.py
    Introdução ao Else

---
+++ LTPC1-2020-2/Pratica-04/programa-03.py
@@ -0,0 +1,9 @@
+#Verificacao de senha com ELSE
+senha_secreta = '123456'
+
+senha = input('Informe a sua senha: ')
+
+if senha == senha_secreta:
+  print('Bem vindo User')
+else:
+  print('Acesso negado!')
