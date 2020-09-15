512b0fac86c7f18afbcc2b3b6496fa1b101ceee5
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2020-09-15 08:13:11 -0300

    Create programa-01.py
    Introdução ao IF

---
+++ LTPC1-2020-2/Pratica-04/programa-01.py
@@ -0,0 +1,8 @@
+#Introducao ao IF
+idade = int(input('Informe sua idade: '))
+
+if idade >= 18:
+  print('Cuidado, agora voce pode ser preso')
+  print('Nao faca besteiras')
+
+print('Fim do programa')
