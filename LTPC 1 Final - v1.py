6767d6a72f5b30cf36fadcf823212be8ea2855db
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2020-11-19 19:49:16 -0300

    Update LTPC 1 Final - v1.py

--- LTPC 1 Final - v1.py
+++ LTPC 1 Final - v1.py
@@ -1,10 +1,16 @@
+#por enquanto
 user_1 = "user 1"
 user_2 = "user 2"
 user_3 = "user 3"
-#por enquanto
+
 senha_1 = "senha 1"
 senha_2 = "senha 2"
 senha_3 = "senha 3"
+
+saldo_1 = 1000
+saldo_2 = 1000
+saldo_3 = 1000
+#por enquanto
 
 def f_menu():
   print("Bem vindo ao banco virtual! Escolha Uma opção para continuar: ")
@@ -18,13 +24,28 @@ def f_user():
   return (vf_user, vf_senha)
 
 def f_verify(vf_user, vf_senha):
+  
   if vf_user == user_1:
     if vf_senha == senha_1:
       c_login = True
     else:
       c_login = False
+  
+  elif vf_user == user_2:
+    if vf_senha == senha_2:
+      c_login = True
+    else:
+      c_login = False
+  
+  elif vf_user == user_3:
+    if vf_senha == senha_3:
+      c_login = True
+    else:
+      c_login = False
+  
   else:
     c_login = False
+  
   return c_login
 
 #main
@@ -37,7 +58,7 @@ while c_login == False:
   c_login = f_verify(v_user, v_senha)
   if c_login == False:
     print("Usuário incorreto!")
-    v_resp = input("Deseja continuar? (s/n)")
+    v_resp = input("Deseja continuar? (s/n): ")
     os.system("clear")
     if v_resp == "n":
       c_login = True
