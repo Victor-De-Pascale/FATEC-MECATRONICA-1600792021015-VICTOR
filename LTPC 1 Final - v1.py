2b65f524bb819e27ee6b2fac405ed8729830a951
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2020-11-19 19:09:51 -0300

    Update LTPC 1 Final - v1.py

--- LTPC 1 Final - v1.py
+++ LTPC 1 Final - v1.py
@@ -1,10 +1,10 @@
-v1_user = "user 1"
-v2_user = "user 2"
-v3_user = "user 3"
+user_1 = "user 1"
+user_2 = "user 2"
+user_3 = "user 3"
 #por enquanto
-v1_senha = "senha 1"
-v2_senha = "senha 2"
-v3_senha = "senha 3"
+senha_1 = "senha 1"
+senha_2 = "senha 2"
+senha_3 = "senha 3"
 
 def f_menu():
   print("Bem vindo ao banco virtual! Escolha Uma opção para continuar: ")
@@ -12,13 +12,31 @@ def f_menu():
   print("2- Pagar transação")
   print("0- Sair")
 
-def f_verify():
-  print("só ppra falar q tem algo aqui")
+def f_user():
+  vf_user = input("Digite seu usuário: ")
+  vf_senha = input("Digite sua senha: ")
+  return (vf_user, vf_senha)
+
+def f_verify(vf_user, vf_senha):
+  if vf_user == user_1:
+    if vf_senha == senha_1:
+      c_login = True
+    else:
+      c_login = False
+  else:
+    c_login = False
+  return c_login
 
 #main
 import os
+c_login = False
 c_continuar = True
-while c_continuar == True:
+
+while c_login == False:
+  v_user, v_senha = f_user()
+  c_login = f_verify(v_user, v_senha)
+
+while c_continuar == False: #fazer teste
   os.system("clear")
   f_menu()
   v_opcao = int(input("Opção: "))
