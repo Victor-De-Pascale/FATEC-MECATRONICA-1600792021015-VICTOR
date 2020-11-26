5a550853057be935f787d6e67f1ef8fb5c3800b5
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2020-11-25 22:20:02 -0300

    Update LTPC 1 Final - v1.py

--- LTPC 1 Final - v1.py
+++ LTPC 1 Final - v1.py
@@ -1,81 +1,123 @@
-#por enquanto
-user_1 = "user 1"
-user_2 = "user 2"
-user_3 = "user 3"
+#1 codigo para cada transaçao especifica, ao inves de qrcode, usar string.
+#quando um usuario fizer um pagamento, o receptor recebera a informacao
+#do valor e da string qrcode.
 
-senha_1 = "senha 1"
-senha_2 = "senha 2"
-senha_3 = "senha 3"
+#cada usuario deve ter o usuario(q sera o nome dele), uma senha e um email.
 
-saldo_1 = 1000
-saldo_2 = 1000
-saldo_3 = 1000
+#cada conta devera ter um ID e um saldo
 
-email_1 = "u@1"
-email_2 = "u@2"
-email_3 = "u@3"
-#por enquanto
+
+
+
+
+import os
+
+xyz_teste_totalnomes = "Victor;ZéGotinha;Michellin;"
+
+#for nomes in xyz_teste_totalnomes.split(";"):
+#  print(nomes.upper())
+#exibe os nomes tudo em maiusculo
+
+xyz_continuar = False
+while xyz_continuar == True:
+  xyz_criarnomes = input("Digite um nome: ")
+  xyz_add = xyz_criarnomes + ";"
+  xyz_teste_totalnomes = xyz_teste_totalnomes + xyz_add
+  print(xyz_teste_totalnomes.upper())
+  input("Pressione qlqr tecla pra continuar...")
+  os.system("clear")
+  xyz_op = input("Deseja continuar adicionando nomes? (s/n) ")
+  if xyz_op != "s":
+    xyz_continuar = False
+#adicionar usuarios ate cansar
+
+#xyz_contador_usuario = 0
+#for caracter in xyz_teste_totalnomes:
+#  if caracter == ";":
+#    xyz_contador_usuario = xyz_contador_usuario + 1
+#print(" ")
+#print(" ")
+#print(" ")
+#print("Existem %.i usuarios" % (xyz_contador_usuario))
+#contador de usuarios
+
+os.system("clear")
+xyz_posicao_inicial = 0
+xyz_posicao_final = 0
+xyz_contador_pev = 0
+xyz_contador = 0
+xyz_user_especifico = ""
+xyz_total_caracteres = len(xyz_teste_totalnomes)
+xyz_indicador = int(input("Indique a posição do ';' para mostrar usuario: "))
+for caracter in xyz_teste_totalnomes:
+  xyz_contador = xyz_contador + 1
+  if caracter == ";":
+    xyz_contador_pev = xyz_contador_pev + 1
+    if xyz_contador_pev == (xyz_indicador - 1):
+      xyz_posicao_inicial = xyz_contador
+    if xyz_contador_pev == xyz_indicador:
+      xyz_posicao_final = xyz_contador
+xyz_usuario_especifico = xyz_teste_totalnomes[xyz_posicao_inicial:(xyz_posicao_final - 1)]
+#print(xyz_usuario_especifico.upper())
+#copiar e colar um nome especifico atraves da indicacao do ";"
+
+
+os.system("clear")
+
+
+
+
+
+v_users = ""
+v_senhas = ""
+v_emails = ""
 
 #incompleto
 def f_menu():
   print("Bem vindo ao banco virtual! Escolha Uma opção para continuar: ")
-  print("1- Requerir transação")
-  print("2- Pagar transação")
+  print("1- Criar/Adicionar Usuário")
+  print("2- Requerir transação")
+  print("3- Pagar transação")
   print("0- Sair")
 
-#acho q ira mudar depois de colocar o "for"
-def f_user():
-  vf_user = input("Digite seu usuário: ")
-  vf_senha = input("Digite sua senha: ")
-  return (vf_user, vf_senha)
-
-#vai mudar porem essa é a base
-def f_verify(vf_user, vf_senha):
-#mudar, esse é versao teste  
-  if (vf_user == user_1) or (vf_user == email_1):
-    if vf_senha == senha_1:
-      c_login = True
-    else:
-      c_login = False
-  
-  elif (vf_user == user_2) or (vf_user == email_2):
-    if vf_senha == senha_2:
-      c_login = True
-    else:
-      c_login = False
-  
-  elif (vf_user == user_3) or (vf_user == email_3):
-    if vf_senha == senha_3:
-      c_login = True
-    else:
-      c_login = False
-  
-  else:
-    c_login = False
-  
-  return c_login
-
 #main
-import os
-c_login = False
-c_menu = True
 c_fim_do_programa = False
-
 while c_fim_do_programa == False:
+  os.system("clear")
+  f_menu()
+  d_op = int(input("Digite uma das opcoes abaixo: "))
   
-  while c_login == False:
-    v_user, v_senha = f_user()
-    c_login = f_verify(v_user, v_senha)
-    if c_login == False:
-      print("Usuário incorreto!")
-      v_resp = input("Deseja continuar? (s/n): ")
+  if d_op == 1:
+    os.system("clear")
+    cf_continuar = True
+    while cf_continuar == True:
+      var_criar_nomes = input("Digite o nome do usuario: ")
+      var_criar_senhas = input("Digite a senha para este usuario: ")
+    
+      var_add_pev = var_criar_nomes + ";"
+      var_add_mail = var_criar_nomes + "@mail;"
+      var_add_senha = var_criar_senhas + ";"
+    
+      v_users = v_users + var_add_pev
+      v_emails = v_emails + var_add_mail
+      v_senhas = v_senhas + var_add_senha
+    
+      print(v_users.upper())
+      print(v_emails.upper())
+      print(v_senhas)
+    
+      input("Pressione qlqr tecla pra continuar...")
       os.system("clear")
-      if v_resp == "n":
-        c_login = True
-        c_menu = False
-        print("Ação cancelada!")
-    else:
-      print("Login feito!")
-      input("Pressione qualquer tecla para continuar...")
+      xyz_op = input("Deseja continuar adicionando nomes? (s/n) ")
+      if xyz_op != "s":
+        cf_continuar = False
+    
+  
+  elif d_op == 0:
+    c_fim_do_programa = True
 
+os.system("clear")
+print(v_users)
+print(v_emails)
+print(v_senhas)
 print("Fim")
