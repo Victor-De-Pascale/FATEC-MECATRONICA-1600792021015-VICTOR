a7b547dd51dee11ba65d10ceeccef4c572406afb
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2020-11-28 19:53:36 -0300

    Update funcao random.py

--- LTPC1-2020-2/Final/funcao random.py
+++ LTPC1-2020-2/Final/funcao random.py
@@ -1,6 +1,12 @@
 #gerar numero aleatorio
 import random
 
+#uniform (float)
 valor = int(random.uniform(1000, 9999))
 
 print(valor)
+
+#randrange (int)
+valor_2 = random.randrange(1000, 9999)
+
+print(valor_2)
