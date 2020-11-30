b14b95ef777a612ff4c69e249c05933b44ee7015
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2020-11-30 15:15:57 -0300

    Delete menu.py

--- LTPC1-2020-2/Final/menu.py
+++
@@ -1,9 +0,0 @@
-while c_continuar == True:
-  os.system("clear")
-  f_menu()
-  v_opcao = int(input("Opção: "))
-  if v_opcao == 0:
-    c_continuar = False
-  else:
-    print("Opção inválida")
-    input("Pressione qualquer tecla para continuar")
