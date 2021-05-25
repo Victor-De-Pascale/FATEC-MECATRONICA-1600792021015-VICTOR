ebb2ae712684eec84930d9d1266c4fbdd092007f
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2021-05-25 09:45:16 -0300

    Update projeto final.py

--- LTPC2-2021-1/projeto final.py
+++ LTPC2-2021-1/projeto final.py
@@ -1,15 +1,16 @@
 import random
 
 op_jogada = {}
-op_jogada[1] = "PEDRA"
-op_jogada[2] = "PAPEL"
-op_jogada[3] = "TESOURA"
-op_jogada[4] = "LAGARTO"
-op_jogada[5] = "SPOCK"
+op_jogada[0] = "PEDRA"
+op_jogada[1] = "PAPEL"
+op_jogada[2] = "TESOURA"
+op_jogada[3] = "LAGARTO"
+op_jogada[4] = "SPOCK"
 
 #exluir td isso aqui
-xyz_nome = False
+xyz_nome = True
 xyz_op = False
+xyz_maquina = True
 #ate aqui
 
 #so pra checar apenas only, excluir ao final
@@ -18,12 +19,19 @@ while xyz_op == True:
     print(f"chave: {op_item} - valor: {op_jogada[op_item]}")
   xyz_op = False
 
-#excluir ao final
+#excluir ao final, comando pro player fazer sua escolha
 while xyz_nome == True:
-  mostrar_op = int(input("Escolha uma opçao ae "))
-  print(op_jogada[mostrar_op])
+  op_player = int(input("Escolha uma opçao ae "))
+  #print(op_jogada[op_player])
   xyz_nome = False
 
-nome_player = input("Digite seu nome ") #variavel nome aqui
+#escolha da maquina com random
+op_maquina = random.choice(op_jogada)
+while xyz_maquina == True:
+  #print(op_maquina)
+  xyz_maquina = False
 
-
+#comparação
+resultado = "EMPATE"
+if op_player == 0:
+  if op_maquina == 
