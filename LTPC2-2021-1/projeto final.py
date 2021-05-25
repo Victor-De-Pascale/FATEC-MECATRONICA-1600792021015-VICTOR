98ac9c6e225b00ead1ec4a48dbc4b8c3cc10ad49
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2021-05-25 12:08:37 -0300

    Update projeto final.py

--- LTPC2-2021-1/projeto final.py
+++ LTPC2-2021-1/projeto final.py
@@ -20,18 +20,48 @@ while xyz_op == True:
   xyz_op = False
 
 #excluir ao final, comando pro player fazer sua escolha
+op_player = int(input("Escolha uma opçao ae "))
 while xyz_nome == True:
-  op_player = int(input("Escolha uma opçao ae "))
-  #print(op_jogada[op_player])
+  print(op_jogada[op_player])
   xyz_nome = False
 
 #escolha da maquina com random
 op_maquina = random.choice(op_jogada)
 while xyz_maquina == True:
-  #print(op_maquina)
+  print(op_maquina)
   xyz_maquina = False
 
+print("")
 #comparação
 resultado = "EMPATE"
-if op_player == 0:
-  if op_maquina == 
+if op_player == 0: #PEDRA
+  if (op_maquina == "PAPEL") or (op_maquina == "SPOCK"):
+    resultado = "DERROTA"
+  elif (op_maquina == "TESOURA") or (op_maquina == "LAGARTO"):
+    resultado = "VITORIA"
+
+elif op_player == 1: #PAPEL
+  if (op_maquina == "TESOURA") or (op_maquina == "LAGARTO"):
+    resultado = "DERROTA"
+  elif (op_maquina == "PEDRA") or (op_maquina == "SPOCK"):
+    resultado = "VITORIA"
+
+elif op_player == 2: #TESOURA
+  if (op_maquina == "PEDRA") or (op_maquina == "SPOCK"):
+    resultado = "DERROTA"
+  elif (op_maquina == "PAPEL") or (op_maquina == "LAGARTO"):
+    resultado = "VITORIA"
+
+elif op_player == 3: #LAGARTO
+  if (op_maquina == "PEDRA") or (op_maquina == "TESOURA"):
+    resultado = "DERROTA"
+  elif (op_maquina == "PAPEL") or (op_maquina == "SPOCK"):
+    resultado = "VITORIA"
+
+elif op_player == 4: #SPOCK
+  if (op_maquina == "PAPEL") or (op_maquina == "LAGARTO"):
+    resultado = "DERROTA"
+  elif (op_maquina == "PEDRA") or (op_maquina == "TESOURA"):
+    resultado = "VITORIA"
+
+print(resultado)
