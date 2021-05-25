7b304cb2880fba2bf23b8c7e941c1e0c31bb7783
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2021-05-25 12:14:53 -0300

    Update projeto final.py

--- LTPC2-2021-1/projeto final.py
+++ LTPC2-2021-1/projeto final.py
@@ -13,25 +13,12 @@ xyz_op = False
 xyz_maquina = True
 #ate aqui
 
-#so pra checar apenas only, excluir ao final
-while xyz_op == True:
-  for op_item in op_jogada.keys():
-    print(f"chave: {op_item} - valor: {op_jogada[op_item]}")
-  xyz_op = False
-
 #excluir ao final, comando pro player fazer sua escolha
 op_player = int(input("Escolha uma opçao ae "))
-while xyz_nome == True:
-  print(op_jogada[op_player])
-  xyz_nome = False
 
 #escolha da maquina com random
 op_maquina = random.choice(op_jogada)
-while xyz_maquina == True:
-  print(op_maquina)
-  xyz_maquina = False
 
-print("")
 #comparação
 resultado = "EMPATE"
 if op_player == 0: #PEDRA
@@ -64,4 +51,7 @@ elif op_player == 4: #SPOCK
   elif (op_maquina == "PEDRA") or (op_maquina == "TESOURA"):
     resultado = "VITORIA"
 
+print("")
 print(resultado)
+print(f"Seu adversário escolheu {op_maquina}...")
+print(f"Voce escolheu {op_jogada[op_player]}...")
