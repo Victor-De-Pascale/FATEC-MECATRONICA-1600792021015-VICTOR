de7cff47969fce09d68092d2cdd5219575d1f06c
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2021-05-30 09:50:04 -0300

    Create treino.py

---
+++ LTPC2-2021-1/treino.py
@@ -0,0 +1,54 @@
+import random
+
+op_jogada = {}
+op_jogada[0] = "PEDRA"
+op_jogada[1] = "PAPEL"
+op_jogada[2] = "TESOURA"
+op_jogada[3] = "LAGARTO"
+op_jogada[4] = "SPOCK"
+
+def vamosJogar():
+  player_joga = int(input("Escolha sua jogada: "))
+  maquina_joga = random.randint(0, 4)
+  return player_joga, maquina_joga
+
+def verificaJogada(op_player, op_maquina):
+  resultado = "EMPATE"
+  if op_player == 0: #PEDRA
+    if (op_maquina == 1) or (op_maquina == 4):
+      resultado = "DERROTA"
+    elif (op_maquina == 2) or (op_maquina == 3):
+      resultado = "VITORIA"
+
+  elif op_player == 1: #PAPEL
+    if (op_maquina == 2) or (op_maquina == 3):
+      resultado = "DERROTA"
+    elif (op_maquina == 0) or (op_maquina == 4):
+      resultado = "VITORIA"
+
+  elif op_player == 2: #TESOURA
+    if (op_maquina == 0) or (op_maquina == 4):
+      resultado = "DERROTA"
+    elif (op_maquina == 1) or (op_maquina == 3):
+      resultado = "VITORIA"
+
+  elif op_player == 3: #LAGARTO
+    if (op_maquina == 0) or (op_maquina == 2):
+      resultado = "DERROTA"
+    elif (op_maquina == 1) or (op_maquina == 4):
+      resultado = "VITORIA"
+
+  elif op_player == 4: #SPOCK
+    if (op_maquina == 1) or (op_maquina == 3):
+      resultado = "DERROTA"
+    elif (op_maquina == 0) or (op_maquina == 2):
+      resultado = "VITORIA"
+  return resultado
+
+op_player, op_maquina = vamosJogar()
+result = verificaJogada(op_player, op_maquina)
+
+print("")
+print(result)
+print(f"Seu adversário escolheu {op_jogada[op_maquina]}...")
+print(f"Voce escolheu {op_jogada[op_player]}...")
