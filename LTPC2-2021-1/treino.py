d6013f8969505c97e799a7b914b4d451b9943b86
Victor-De-Pascale <70581651+Victor-De-Pascale@users.noreply.github.com>
2021-05-30 20:43:13 -0300

    Update treino.py

--- LTPC2-2021-1/treino.py
+++ LTPC2-2021-1/treino.py
@@ -8,6 +8,11 @@ op_jogada[2] = "TESOURA"
 op_jogada[3] = "LAGARTO"
 op_jogada[4] = "SPOCK"
 
+hist_player = []
+hist_maquina = []
+hist_confronto = {}
+
+conta_rodadas = 0
 rodajogo = True
 
 def vamosJogar(op_item):
@@ -25,6 +30,12 @@ def vamosJogar(op_item):
   maquina_joga = random.randint(0, 4)
   return player_joga, maquina_joga
 
+def montarHistorico(contador, resultado, c_player, c_maquina, c_jogada, listaplayer, listamaquina, chavehistorico):
+  listaplayer.append(c_jogada[c_player])
+  listamaquina.append(c_jogada[c_maquina])
+  chavehistorico[f"JOGO {contador}"] = f"{resultado}"
+  return listaplayer, listamaquina, chavehistorico
+
 def menuMain():
   os.system("clear")
   print("BEM VINDO AO EXPERT JOKENPO!")
@@ -40,11 +51,16 @@ def mostrarResultado(resultado, c_player, c_maquina, c_jogada):
   print(f"Voce escolheu {c_jogada[c_player]}...")
   input("")
 
-def mostrarHistorico():
-  print("")
-
-def verificaJogada(op_player, op_maquina):
+def mostrarHistorico(listaplayer, listamaquina, chavehistorico):
+  print(listaplayer)
+  print(listamaquina)
+  print(chavehistorico)
+  input("")
+  
+def verificaJogada(op_player, op_maquina, cont):
   resultado = "EMPATE"
+  cont = cont + 1
+
   if op_player == 0: #PEDRA
     if (op_maquina == 1) or (op_maquina == 4):
       resultado = "DERROTA"
@@ -74,19 +90,40 @@ def verificaJogada(op_player, op_maquina):
       resultado = "DERROTA"
     elif (op_maquina == 0) or (op_maquina == 2):
       resultado = "VITORIA"
-  return resultado
+
+  return resultado, cont
 
 while rodajogo == True:
   menuMain()
+  print("")
   op_menu = int(input("Sua opção: "))
+  
   if op_menu == 0:
+    os.system("clear")
+    print("Obrigado por jogar!")
     rodajogo = False
+  
   elif op_menu == 1:
+    #Passo 1 - Mecanica
     op_player, op_maquina = vamosJogar(op_jogada)
-    result = verificaJogada(op_player, op_maquina)
+    #Passo 2 - Comparaçao
+    result, conta_rodadas = verificaJogada(op_player, op_maquina, conta_rodadas)
+    #Passo 3 - Retorno do resultado
     mostrarResultado(result, op_player, op_maquina, op_jogada)
+    #Passo 4 - Construção do histórico
+    hist_player, hist_maquina, hist_confronto = montarHistorico(conta_rodadas, result, op_player, op_maquina, op_jogada, hist_player, hist_maquina, hist_confronto)
+    #Victor diz: sim, isso tudo é uma linha só
+  
   elif op_menu == 2:
-    print("")
+    os.system("clear")
+    if conta_rodadas == 0:
+      print("Não houve partidas jogadas!")
+      input("")
+    else:
+      mostrarHistorico(hist_player, hist_maquina, hist_confronto)
+  
   else:
     print("Opção inválida!")
     input("")
+
+#Fim
